adbf1de46bfc2b17b950da8febf47d4985dfb13c
yozu <yasuo@ozu.email>
2021-02-04 03:32:09 +0900

    Add local_field to solution

--- src/annealers/src/solution.rs
+++ src/annealers/src/solution.rs
@@ -1,6 +1,8 @@
 use crate::model::SingleModelView;
 use crate::node::{Node, SingleNode};
 use crate::repr::BinaryRepr;
+use crate::set::NodeSet;
+use crate::variable::Real;
 use std::marker::PhantomData;
 
 pub trait Solution {
@@ -12,6 +14,7 @@ pub struct SingleSolution<NodeType: SingleNode> {
 	pub state: BinaryRepr,
 	pub energy: Option<NodeType::RealType>,
 	pub occurrences: usize,
+	pub local_field: Option<Vec<NodeType::RealType>>,
 	_phantom: PhantomData<NodeType>,
 }
 
@@ -30,11 +33,13 @@ impl<M: SingleNode> SingleSolution<M> {
 		Self {
 			state,
 			energy: None,
+			local_field: None,
 			occurrences: 1,
 			_phantom: PhantomData,
 		}
 	}
 
+	#[inline]
 	/// Get the number of solutions
 	pub fn len(&self) -> usize {
 		self.state.len()
@@ -49,6 +54,12 @@ impl<M: SingleNode> SingleSolution<M> {
 		}
 	}
 
+	/// Ensure that SingleSolution has local field.
+	pub fn with_local_field<P: SingleModelView<Node = M>>(mut self, model: &P) -> Self {
+		self.local_field = Some(self.clone().calculate_local_field(model));
+		self
+	}
+
 	/// Ensure that SingleSolution has energy.
 	pub fn with_energy<P: SingleModelView<Node = M>>(mut self, model: &P) -> Self {
 		self.energy = Some(self.calculate_energy(model));
@@ -56,10 +67,7 @@ impl<M: SingleNode> SingleSolution<M> {
 	}
 
 	/// Calculate energy with model.
-	pub fn calculate_energy<P: SingleModelView<Node = M>>(
-		&self,
-		model: &P,
-	) -> <P::Node as Node>::RealType {
+	pub fn calculate_energy<P: SingleModelView<Node = M>>(&self, model: &P) -> M::RealType {
 		if let Some(e) = self.energy {
 			e
 		} else {
@@ -68,6 +76,28 @@ impl<M: SingleNode> SingleSolution<M> {
 				energy += model.get_weight(&prod) * model.calculate_prod(&prod, self);
 			}
 			energy
+		}
+	}
+
+	/// Calculate local field with model.
+	pub fn calculate_local_field<P: SingleModelView<Node = M>>(
+		mut self,
+		model: &P,
+	) -> Vec<M::RealType> {
+		if let Some(v) = self.local_field {
+			v
+		} else {
+			let mut res = vec![<M::RealType as Real>::from_i32(0); self.len()];
+			for prod in model.prods() {
+				for node in prod.iter() {
+					self.state.flip(node);
+					let a = model.calculate_prod(&prod, &self);
+					self.state.flip(node);
+					let b = model.calculate_prod(&prod, &self);
+					res[node] += (a - b) * model.get_weight(&prod);
+				}
+			}
+			res
 		}
 	}
 
